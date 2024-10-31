73e04dee71bcaa20c75e02d0ca6862661ffb7465
Khaled-J7 <6711@holbertonstudents.com>
2024-10-31 16:55:36 +0100

    Update the App.js File

--- frontend/App.js
+++ frontend/App.js
@@ -1,30 +1,36 @@
-import 'react-native-gesture-handler';  // This must be the first import
+import 'react-native-gesture-handler';  // Must be first import
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './src/screens/auth/SplashScreen';
 import LoginSignupScreen from './src/screens/auth/LoginSignupScreen';
+import HomeScreen from './src/screens/main/HomeScreen';  // We'll create this
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
+      <Stack.Navigator
         initialRouteName="Splash"
-        screenOptions={{ 
+        screenOptions={{
           headerShown: false,
-          gestureEnabled: false 
+          gestureEnabled: false
         }}
       >
-        <Stack.Screen 
-          name="Splash" 
-          component={SplashScreen} 
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+          options={{ gestureEnabled: false }}
+        />
+        <Stack.Screen
+          name="LoginSignup"
+          component={LoginSignupScreen}
           options={{ gestureEnabled: false }}
         />
         <Stack.Screen 
-          name="LoginSignup" 
-          component={LoginSignupScreen}
+          name="Home"
+          component={HomeScreen}
           options={{ gestureEnabled: false }}
         />
       </Stack.Navigator>
